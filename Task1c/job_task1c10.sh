1dcb865a8230b01cd01df0d113cd61f82a29261a
mukul <mukul312@gmail.com>
2017-03-06 21:51:08 -0600

    parallel recursive

---
+++ Task1c/job_task1c10.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -J MatMul10        # Job Name
+#SBATCH -o Test.o%j    # Output and error file name (%j expands to jobID)
+#SBATCH -n 10           # Total number of  tasks requested
+#SBATCH -p normal  # Queue (partition) name -- normal, development, etc.
+#SBATCH -t 05:30:00     # Run time (hh:mm:ss) - 2.5 hours
+rm -f Task_1c10
+touch Task_1c10
+./matmul_par_ikj  13 >> Task_1c10
---
+++ Task1c/job_task1c13.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -J MatMul13        # Job Name
+#SBATCH -o Test.o%j    # Output and error file name (%j expands to jobID)
+#SBATCH -n 13           # Total number of  tasks requested
+#SBATCH -p normal  # Queue (partition) name -- normal, development, etc.
+#SBATCH -t 05:30:00     # Run time (hh:mm:ss) - 2.5 hours
+rm -f Task_1c13 
+touch Task_1c13
+./matmul_par_ikj  13 >> Task_1c13
