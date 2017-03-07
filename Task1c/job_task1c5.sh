b0281a1ec0dcfca292bd84746d727a789a25e339
mukul <mukul312@gmail.com>
2017-03-06 22:34:30 -0600

    newscript

--- Task1c/job_task1c5.sh
+++ Task1c/job_task1c5.sh
@@ -2,7 +2,7 @@
 #SBATCH -J MatMul        # Job Name
 #SBATCH -o Test.o%j    # Output and error file name (%j expands to jobID)
 #SBATCH -n 5           # Total number of  tasks requested
-#SBATCH -p normal  # Queue (partition) name -- normal, development, etc.
+#SBATCH -p development  # Queue (partition) name -- normal, development, etc.
 #SBATCH -t 05:30:00     # Run time (hh:mm:ss) - 5.5 hours
 rm -f Task_1c5 
 touch Task_1c5
--- Task1c/job_task1c6.sh
+++ Task1c/job_task1c6.sh
@@ -2,7 +2,7 @@
 #SBATCH -J MatMul        # Job Name
 #SBATCH -o Test.o%j    # Output and error file name (%j expands to jobID)
 #SBATCH -n 6           # Total number of  tasks requested
-#SBATCH -p normal  # Queue (partition) name -- normal, development, etc.
+#SBATCH -p development  # Queue (partition) name -- normal, development, etc.
 #SBATCH -t 05:30:00     # Run time (hh:mm:ss) - 2.5 hours
 rm -f Task_1c6 
 touch Task_1c6
