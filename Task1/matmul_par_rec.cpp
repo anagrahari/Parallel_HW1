62a8db9089009b3d42985322a3373b860da64d45
mukul <mukul312@gmail.com>
2017-03-13 16:19:12 -0500

    dsd

--- Task1/matmul_par_rec.cpp
+++ Task1/matmul_par_rec.cpp
@@ -1,51 +1,83 @@
 #include <iostream>
+#include <boost/numeric/mtl/mtl.hpp>
+#include <boost/numeric/mtl/recursion/matrix_recursator.hpp>
 #include <cilk/cilk.h>
+#include <time.h>
 #include <sys/time.h>
-#include <math.h>
-#include <cstdlib>
-#include "matmul.h"
-using namespace std;
 
-void matMultiplyRec(int Zx0, int Zxn, int Zy0,int Zyn, int Xx0, int Xxn, int Xy0, int Xyn, int Yx0, int Yxn, int Yy0,int  Yyn, int n, int baseno) {
-	if(n == baseno) {
-		for(int k =0;k < n; k++ ) {
-			for(int i = Zx0; i <= Zxn; i++) {
-				for(int j = Zy0; j<= Zyn; j++) {
-					Z[i][j] = Z[i][j] + X[i][Xy0 + k] * Y[Yx0 + k][j];
-				}
-			}
-		}
-	}
-	else {
-		cilk_spawn matMultiplyRec(Zx0,Zx0 +(n/2 -1),Zy0,Zy0 +(n/2 -1),Xx0,Xx0 +(n/2 -1),Xy0,Xy0 +( n/2 -1),Yx0,Yx0 +( n/2 -1),Yy0,Yy0 +( n/2 -1),n/2 , baseno);
-		cilk_spawn matMultiplyRec(Zx0,Zx0 +(n/2 -1),Zy0 + n/2,Zy0 + (n-1),Xx0,Xx0 +(n/2 -1),Xy0,Xy0 +( n/2 -1),Yx0,Yx0 +( n/2 -1),Yy0 + n/2,Yy0 + (n-1),n/2, baseno);
-		cilk_spawn matMultiplyRec(Zx0 + n/2,Zx0 + (n-1),Zy0,Zy0 +( n/2 -1),Xx0 + n/2,Xx0 + (n-1),Xy0,Xy0 +( n/2 -1),Yx0,Yx0 +( n/2 -1),Yy0,Yy0 +( n/2 -1),n/2, baseno);
-		matMultiplyRec(Zx0 + n/2,Zx0 + (n-1),Zy0 + n/2,Zy0 + (n-1),Xx0 + n/2,Xx0 + (n-1),Xy0,Xy0 + (n-1),Yx0,Yx0 +( n/2 -1),Yy0 + n/2,Yy0 + (n-1),n/2, baseno);
-		cilk_sync;
-		cilk_spawn matMultiplyRec(Zx0,Zx0 +(n/2 -1),Zy0,Zy0 +( n/2 -1),Xx0,Xx0 +( n/2 -1),Xy0 + n/2,Xy0 + (n-1),Yx0 + n/2,Yx0 + (n-1),Yy0,Yy0 +( n/2 -1),n/2, baseno);
-		cilk_spawn matMultiplyRec(Zx0,Zx0 +(n/2 -1),Zy0 + n/2,Zy0 + (n-1),Xx0,Xx0 +( n/2 -1),Xy0 + n/2,Xy0 + (n-1),Yx0 + n/2,Yx0 + (n-1),Yy0 + n/2,Yy0 + (n-1),n/2, baseno);
-		cilk_spawn matMultiplyRec(Zx0 + n/2,Zx0 + (n-1),Zy0,Zy0 +( n/2 -1),Xx0 + n/2,Xx0 + (n-1),Xy0 + n/2,Xy0 + (n-1),Yx0 + n/2,Yx0 + (n-1),Yy0,Yy0 +( n/2 -1),n/2, baseno);
-		matMultiplyRec(Zx0 + n/2,Zx0 + (n-1),Zy0 + n/2,Zy0 + (n-1),Xx0 + n/2,Xx0 + (n-1),Xy0 + n/2,Xy0 + (n-1),Yx0 + n/2,Yx0 + (n-1),Yy0 + n/2,Yy0 + (n-1),n/2, baseno);        
-		cilk_sync;
+using namespace mtl; using std::cout;
+typedef morton_dense<double, recursion::morton_z_mask>  matrix_type;
 
+
+void matMulParRec(mat::recursator<matrix_type> Z, mat::recursator<matrix_type> X,
+		mat::recursator<matrix_type> Y, int BASENO) {
+	matrix_type z = *Z;
+	matrix_type x = *X;
+	matrix_type y = *Y;
+
+	if (num_rows(z) == BASENO && num_cols(z) == BASENO) {
+		for(int k = 0; k < BASENO; k++)
+			for(int i = 0; i < BASENO; i++)
+				for(int j = 0; j< BASENO; j++)
+					z[i][j] = z[i][j] +  (x[i][k] * y[k][j]);
+	} else {
+		cilk_spawn matMulParRec(north_west(Z), north_west(X), north_west(Y), BASENO);	
+		cilk_spawn matMulParRec(north_east(Z), north_west(X), north_east(Y), BASENO);	
+		cilk_spawn matMulParRec(south_west(Z), south_west(X), north_west(Y), BASENO);	
+			   matMulParRec(south_east(Z), south_west(X), north_east(Y), BASENO);
+		sync;
+		cilk_spawn matMulParRec(north_west(Z), north_east(X), south_west(Y), BASENO);	
+		cilk_spawn matMulParRec(north_east(Z), north_east(X), south_east(Y), BASENO);	
+		cilk_spawn matMulParRec(south_west(Z), south_east(X), south_west(Y), BASENO);	
+			   matMulParRec(south_east(Z), south_east(X), south_east(Y), BASENO);
+		sync;
 	}
 }
 
-int main(int argc, char *argv []) {
-	int baseno = 1 << atoi(argv[1]);
-	
-	struct timeval start,end;
-	MATRIX_SIZE = 1 << 13;
-	cout << "Run with matrix size:" << MATRIX_SIZE << "\n";
-	{
-		init_matrices();
-		cout << "BaseNo: " << baseno << "\n";
-		gettimeofday(&start,NULL); //Start timing the computation
-		matMultiplyRec(0,MATRIX_SIZE-1,0,MATRIX_SIZE-1,0,MATRIX_SIZE-1,0,MATRIX_SIZE-1,0,MATRIX_SIZE-1,0,MATRIX_SIZE-1, MATRIX_SIZE, baseno);
-		gettimeofday(&end,NULL); //Stop timing the computation
-		double myTime = (end.tv_sec+(double)end.tv_usec/1000000) -
-			(start.tv_sec+(double)start.tv_usec/1000000);
-		cout << "Matmul recursive: " << myTime << " seconds.\n";
-		destroy_matrices();
+void init_matrix(mat::recursator<matrix_type> Z, mat::recursator<matrix_type> X,
+		 mat::recursator<matrix_type> Y) {
+	matrix_type z = *Z;
+	matrix_type x = *X;
+	matrix_type y = *Y;
+
+	for (int i = 0; i < num_rows(z); i++) {
+		for (int j = 0; j < num_rows(z); j++) {
+			x[i][j] = (i + j + 2*i*j);
+			y[i][j] = (i + j + 3*i);
+			z[i][j] = 0;
+		}
 	}
+
+}
+
+int main(int argc, char** args)
+{
+	struct timeval start,end;
+	int MATRIX_SIZE = 1 << atoi(args[1]);
+	int BASENO = 1 << atoi(args[2]);
+	matrix_type  Z(MATRIX_SIZE, MATRIX_SIZE);
+	matrix_type  X(MATRIX_SIZE, MATRIX_SIZE);
+	matrix_type  Y(MATRIX_SIZE, MATRIX_SIZE);
+
+	mat::recursator<matrix_type>    recz(Z);
+	mat::recursator<matrix_type>    recx(X);
+	mat::recursator<matrix_type>    recy(Y);
+
+	cout << "Matrix row,columns: " << MATRIX_SIZE << "\tBase Case: " << BASENO <<"\n";
+
+	init_matrix(recz, recx, recy);
+	
+	gettimeofday(&start,NULL); //Start timing the computation
+	matMulParRec(recz, recx, recy, BASENO);
+	gettimeofday(&end,NULL); //Stop timing the computation
+
+	double myTime = (end.tv_sec+(double)end.tv_usec/1000000) -
+			 (start.tv_sec+(double)start.tv_usec/1000000);
+
+	cout << "Total time: " << myTime << " seconds.\n";
+	/*cout << *recx<< "\n";
+	cout << *recy << "\n";
+	cout << *recz << "\n";
+	*/
+	return 0;
 }
